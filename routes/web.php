25910b4631121da3bb14f1759cc48d269da15a49
Nariman <nngasanov@gmail.com>
2021-01-27 14:58:42 +0500

    Add swagger docs

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/swagger', [\App\Http\Controllers\SwaggerController::class, 'index']);
